1e6bae8c2213c317ebdfba295c94a39fedaab634
pradnya1105 <prdnsamant.11@gmail.com>
2025-02-06 22:41:06 +0530

    linked account page with privatelayout.master page

--- TrendBlend/pages/Account.aspx.designer.cs
+++ TrendBlend/pages/Account.aspx.designer.cs
@@ -11,16 +11,7 @@ namespace TrendBlend.pages
 {
 
 
-    public partial class Account
+    public partial class WebForm2
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
