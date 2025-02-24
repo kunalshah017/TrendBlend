c0f2c4e5a4abb542c78f2f797f17e2fd5ebdda96
kunalshah017 <kunalmanishshah@gmail.com>
2025-02-24 22:44:18 +0530

    Added Settings Page UI & Open Graph Link Changed

--- TrendBlend/pages/Settings.aspx
+++ TrendBlend/pages/Settings.aspx
@@ -2,7 +2,47 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
     <title>TrendBlend | Settings</title>
+    <link href="/styles/Settings/styles.css" rel="stylesheet" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
-    <div></div>
+    <div class="main_container">
+        <div class="settings_container">
+            <div class="setting_item">
+                <div class="setting_header" onclick="togglePasswordReset()">
+                    <span>Reset Password</span>
+                    <i class="fa fa-chevron-down"></i>
+                </div>
+                <div class="setting_content" id="passwordResetContent">
+                    <div class="input_group">
+                        <asp:TextBox ID="NewPasswordInput" runat="server" TextMode="Password"
+                            CssClass="settings_input" placeholder="New Password" />
+                    </div>
+                    <div class="input_group">
+                        <asp:TextBox ID="ConfirmPasswordInput" runat="server" TextMode="Password"
+                            CssClass="settings_input" placeholder="Confirm Password" />
+                    </div>
+                    <asp:Label ID="PasswordErrorLabel" runat="server" CssClass="error_message" Visible="false" />
+                    <asp:Button ID="ResetPasswordButton" runat="server" Text="Reset Password"
+                        CssClass="settings_button" OnClick="ResetPasswordButton_Click" />
+                </div>
+            </div>
+
+            <div class="logout_container">
+                <p>Logout of TrendBlend</p>
+                <asp:Button ID="LogoutButton" runat="server" Text="Logout"
+                    CssClass="settings_button logout_button" OnClick="LogoutButton_Click" />
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        function togglePasswordReset() {
+            const content = document.getElementById('passwordResetContent');
+            const header = content.previousElementSibling;
+            const icon = header.querySelector('i');
+
+            content.classList.toggle('expanded');
+            icon.classList.toggle('rotated');
+        }
+    </script>
 </asp:Content>
--- TrendBlend/pages/Settings.aspx.cs
+++ TrendBlend/pages/Settings.aspx.cs
@@ -1,17 +1,114 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace TrendBlend.pages
 {
-	public partial class Settings : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+    public partial class Settings : System.Web.UI.Page
+    {
+        private readonly string cs = ConfigurationManager.ConnectionStrings["TrendBlendDB"].ConnectionString;
 
-		}
-	}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["UserName"] == null)
+                {
+                    Response.Redirect("~/pages/SignIn.aspx");
+                }
+            }
+        }
+
+        private string HashPassword(string password)
+        {
+            string salt = ConfigurationManager.AppSettings["PasswordSalt"];
+            string saltedPassword = string.Concat(password, salt);
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(saltedPassword);
+                byte[] hash = sha256.ComputeHash(bytes);
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    builder.Append(hash[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        protected void ResetPasswordButton_Click(object sender, EventArgs e)
+        {
+            string newPassword = NewPasswordInput.Text;
+            string confirmPassword = ConfirmPasswordInput.Text;
+
+            if (string.IsNullOrEmpty(newPassword) || string.IsNullOrEmpty(confirmPassword))
+            {
+                PasswordErrorLabel.Text = "Please fill in both password fields";
+                PasswordErrorLabel.Visible = true;
+                return;
+            }
+
+            if (newPassword != confirmPassword)
+            {
+                PasswordErrorLabel.Text = "Passwords do not match";
+                PasswordErrorLabel.Visible = true;
+                return;
+            }
+
+            string hashedPassword = HashPassword(newPassword);
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                string query = "UPDATE Users SET Password = @Password WHERE UserName = @Username";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Password", hashedPassword);
+                cmd.Parameters.AddWithValue("@Username", Session["UserName"].ToString());
+
+                try
+                {
+                    con.Open();
+                    int result = cmd.ExecuteNonQuery();
+                    if (result > 0)
+                    {
+                        // Clear the password fields
+                        NewPasswordInput.Text = "";
+                        ConfirmPasswordInput.Text = "";
+
+                        // Show success message
+                        PasswordErrorLabel.Text = "Password updated successfully";
+                        PasswordErrorLabel.CssClass = "success_message";
+                        PasswordErrorLabel.Visible = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    PasswordErrorLabel.Text = "Error updating password";
+                    PasswordErrorLabel.Visible = true;
+                }
+            }
+        }
+
+        protected void LogoutButton_Click(object sender, EventArgs e)
+        {
+            // Clear cookie if exists
+            if (Request.Cookies["UserInfo"] != null)
+            {
+                HttpCookie userCookie = new HttpCookie("UserInfo");
+                userCookie.Expires = DateTime.Now.AddDays(-1);
+                Response.Cookies.Add(userCookie);
+            }
+
+            // Clear session
+            Session.Clear();
+
+            // Redirect to login
+            Response.Redirect("~/pages/SignIn.aspx");
+        }
+    }
 }
